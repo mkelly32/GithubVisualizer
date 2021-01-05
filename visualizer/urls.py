759b239d79d8d60003ef53bd1558d92566af3a6a
Mike Kelly <mkelly32@tcd.ie>
2021-01-05 03:44:12 +0000

    laid foundations for web app

--- visualizer/urls.py
+++ visualizer/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', views.user, name='user'),
+    path('', views.repo, name='repo'),
 ]
--- visualizer/views.py
+++ visualizer/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 def home(request):
     context = {}
     return render(request, 'home.html', context)
+    
+def user(request):
+    context = {}
+    return render(request, 'user.html', context)
+
+def repo(request):
+    context = {}
+    return render(request, 'repo.html', context)
